3be073e24b9792dd590dcd788f8fe9a71e5e230e
Chris Lord <chrislord.net@gmail.com>
2014-06-09 20:09:26 +0100

    Bug 1022713 - Only render the necessary icons when dragging. r=
    Dragging an icon previously caused all icons from the first icon that needs
    re-rendering to the end of the grid. This makes sure that only the icons
    that are affected by the rearrangement are re-rendered.

--- shared/elements/gaia_grid/js/grid_view.js
+++ shared/elements/gaia_grid/js/grid_view.js
@@ -215,25 +215,40 @@
      * on the grid.
      * @param {Object} options Options to render with including:
      *  - from {Integer} The index to start rendering from.
+     *  - to {Integer} The index to end rendering at.
      *  - skipDivider {Boolean} Whether or not to skip the divider
      */
     render: function(options) {
       var self = this;
       options = options || {};
 
+      // Bounds-check the 'from' and 'to' parameters.
+      var from =
+        Math.max(0, Math.min(this.items.length - 1, options.from || 0));
+      var to =
+        Math.max(0, Math.min(this.items.length - 1,
+          (options.to === 0) ? 0 : options.to || this.items.length - 1));
+      if (to < from) {
+        to = from;
+      }
+
+      // If we're rendering to the end of the grid, refresh the container height
+      var setContainerHeight = false;
+      if (to == this.items.length - 1) {
+        setContainerHeight = true;
+      }
+
+      // Store the from and to indices as items, as removing placeholders/
+      // cleaning will alter indexes.
+      var fromItem = this.items[from];
+      var toItem = this.items[to];
+
       this.removeAllPlaceholders();
       this.cleanItems(options.skipDivider);
 
-      // Start rendering from one before the drop target. If not,
-      // we may drop over the divider and miss rendering an icon.
-      var from = options.from - 1 || 0;
-
-      // TODO This variable should be an argument of this method. See
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=1010742#c4
-      var to = this.items.length - 1;
-
-      // Reset offset steps
-      this.layout.offsetY = 0;
+      // Reset the to index until we find it again when iterating over items
+      // below.
+      to = this.items.length - 1;
 
       // Grid render coordinates
       var x = 0;
@@ -249,6 +264,9 @@
         x = 0;
         y++;
       }
+
+      // Reset offset steps
+      this.layout.offsetY = 0;
 
       for (var idx = 0; idx <= to; idx++) {
         var item = this.items[idx];
@@ -273,7 +291,17 @@
           step(lastItem);
         }
 
-        if (idx >= from) {
+        if (item == fromItem) {
+          from = idx;
+        }
+        if (item == toItem) {
+          to = idx;
+        }
+
+        // Check if idx is >= to also - there's a chance that decrementing from
+        // at the beginning of this function means fromItem is a placeholder
+        // or a removed divider.
+        if (idx >= from || idx >= to) {
           item.render([x, y], idx);
         }
 
@@ -286,6 +314,9 @@
       }
 
       this.element.setAttribute('cols', this.layout.cols);
+
+      // Reset offsetY as stepYAxis changes it and it may be needed elsewhere.
+      this.layout.offsetY = 0;
     }
   };
 
