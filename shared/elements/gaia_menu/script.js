93f3b3414ca3f2fd436ca70b91b9ec1b437b1a78
Chris Lord <chrislord.net@gmail.com>
2014-10-06 17:09:08 +0100

    Bug XXX - Add transitions to GaiaMenu (untested)

--- shared/elements/gaia_menu/script.js
+++ shared/elements/gaia_menu/script.js
@@ -16,8 +16,18 @@ window.GaiaMenu = (function(win) {
 
     var cancelButton = this._template.querySelector('.gaia-menu-cancel');
 
+    this.cancelPressed = false;
     cancelButton.addEventListener('click', function () {
       this.hide();
+      this.cancelPressed = true;
+    }.bind(this));
+
+    this.addEventListener('transitionend', function() {
+      if (!this.cancelPressed) {
+        return;
+      }
+
+      this.cancelPressed = false;
       this.dispatchEvent(new CustomEvent('gaiamenu-cancel'));
     }.bind(this));
 
@@ -25,6 +35,10 @@ window.GaiaMenu = (function(win) {
 
     ComponentUtils.style.call(this, baseurl);
     navigator.mozL10n.ready(this.localize.bind(this));
+
+    // Force a style flush so that calling show() immediately doesn't skip
+    // transitions.
+    this.clientTop;
   };
 
   proto.localize = function() {
@@ -43,7 +57,7 @@ window.GaiaMenu = (function(win) {
   var template = document.createElement('template');
 
   template.innerHTML =
-    `<form role="dialog" data-type="action">
+    `<form role="dialog" data-type="action" hidden>
       <content select="header"></content>
       <menu>
         <content select="button"></content>
